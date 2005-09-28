1254fc4d6046fdc902e70c3451586ebca9876aa1
asolar <none@none>
2005-09-28 10:22:20 -0700

    Minor changes

--- src/SketchSolver/driver.cpp
+++ src/SketchSolver/driver.cpp
@@ -25,37 +25,32 @@ void BitSwap::defineSketch(SAT_Manager mng, varDir& dir){
 	dir.declareArr("Tsp", N);
 	dir.declareArr("Tsm", N);
 	
-
-	
-	cout<<" t1[0] = in[0] & c[0]; "<<endl;
-	addAndClause(mng, dir.getArr("T1", 0), dir.getArr(IN, 0), dir.getArr("C" ,0));
-
+	cout<<" Tsm = IN << shamt;"<<endl;
 	shiftArrNdet(mng, dir, "Tsm", IN, "shamt", RIGHT);
 
 
 	// t1'[i] = in[i] xor Tsm[i];
-	cout<<" t1'[i] = in[i] xor Tsm[i]; "<<endl;
-	for(int i=1; i<N; ++i){
+	cout<<" t1p = in xor Tsm; "<<endl;
+	for(int i=0; i<N; ++i){
 		addXorClause(mng, dir.getArr("T1p", i), dir.getArr(IN, i), dir.getArr("Tsm" ,i));
 	}
 	// t1[i] = t1p[i] & c[i]
-	cout<<" t1[i] = t1p[i] & c[i] "<<endl;
-	for(int i=1; i<N; ++i){
+	cout<<" t1 = t1p & c "<<endl;
+	for(int i=0; i<N; ++i){
 		addAndClause(mng, dir.getArr("T1", i), dir.getArr("T1p", i), dir.getArr("C" ,i));
 	}
 
+	cout<<"Tsp = t1 >> shamt;"<<endl;
 	shiftArrNdet(mng, dir, "Tsp", "T1", "shamt", LEFT);
 	
 	// t2[i] = t1[i] xor Tsp[i]
-	cout<<" t2[i] = t1[i] xor tsp[i] "<<endl;
-	for(int i=0; i<N-1; ++i){
+	cout<<" t2 = t1 xor tsp "<<endl;
+	for(int i=0; i<N; ++i){
 		addXorClause(mng, dir.getArr("T2", i), dir.getArr("T1", i), dir.getArr("Tsp" ,i));
 	}
-	// t2[N-1] = t1[N-1]
-	addEqualsClause(mng, dir.getArr("T2", N-1), dir.getArr("T1", N-1));		
 	
 	// SOUTq[i] = t2[i] xor in[i];
-	cout<<" SOUT[i] = t2[i] xor in[i]; "<<endl;
+	cout<<" SOUT = t2 xor in; "<<endl;
 	for(int i=0; i<N; ++i){
 		addXorClause(mng, dir.getArr(SOUT, i), dir.getArr("T2", i), dir.getArr(IN ,i));
 	}
@@ -63,10 +58,16 @@ void BitSwap::defineSketch(SAT_Manager mng, varDir& dir){
 
 void BitSwap::defineSpec(SAT_Manager mng, varDir& dir){
 	Dout( cout<<"defineSpec()"<<endl );
-	for(int i=0; i<N; i+=3){
+	for(int i=0; i<N; i+=5){
+//		addEqualsClause(mng, dir.getArr(OUT, i+0), dir.getArr(IN, i+0));	
+//		addEqualsClause(mng, dir.getArr(OUT, i+2), dir.getArr(IN, i+1));	
+//		addEqualsClause(mng, dir.getArr(OUT, i+1), dir.getArr(IN, i+2));	
+//		addEqualsClause(mng, dir.getArr(OUT, i+3), dir.getArr(IN, i+3));	
 		addEqualsClause(mng, dir.getArr(OUT, i+0), dir.getArr(IN, i+0));	
-		addEqualsClause(mng, dir.getArr(OUT, i+2), dir.getArr(IN, i+1));	
-		addEqualsClause(mng, dir.getArr(OUT, i+1), dir.getArr(IN, i+2));
+		addEqualsClause(mng, dir.getArr(OUT, i+3), dir.getArr(IN, i+1));	
+		addEqualsClause(mng, dir.getArr(OUT, i+4), dir.getArr(IN, i+2));
+		addEqualsClause(mng, dir.getArr(OUT, i+1), dir.getArr(IN, i+3));
+		addEqualsClause(mng, dir.getArr(OUT, i+2), dir.getArr(IN, i+4));
 	}
 }
 
@@ -77,7 +78,7 @@ void BitSwap::defineSpec(SAT_Manager mng, varDir& dir){
 
 int main(int argc, char ** argv)
 {
-	int N = 3*atoi(argv[1]);
+	int N = 5*atoi(argv[1]);
 	BitSwap bswap(N, 3);
 	bswap.setup();
 	bswap.solve();
