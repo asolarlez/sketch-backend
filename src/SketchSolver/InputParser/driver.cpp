a351ad0e7fff26dc18d42eb0230c3ab839fc6d5b
asolar <none@none>
2005-10-18 23:27:11 -0700

    *** empty log message ***

--- src/SketchSolver/InputParser/driver.cpp
+++ src/SketchSolver/InputParser/driver.cpp
@@ -52,6 +52,9 @@ int main(int argc, char** argv){
       msg += argv[input_idx];
 
       Assert( INp::functionMap.find(fname) != INp::functionMap.end(),  msg );
+      for(map<string, BooleanDAG*>::iterator it = INp::functionMap.begin(); it != INp::functionMap.end(); ++it){
+      	cout<<it->first<<endl;
+      }
     }
 	return 0;
     }catch(BasicError& be){
