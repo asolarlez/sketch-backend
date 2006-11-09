e47f1b0392824dda0d058d235c6e2a998a2378b1
arnold <none@none>
2006-11-09 08:59:42 -0800

    Some refactoring, including (1) changes to addXyzClause methods in varDir, such that to not take a gid argument (deprecated) and instead take an optional result variable argument, plus code refurbishing; (2) making the solver member of varDir private, hence regulating all access through wrappers; (3) making a few auxiliary functions members of varDir. Consequently, some changes take place in bit-vector operator code.

--- src/SketchSolver/BooleanToCNF.cpp
+++ src/SketchSolver/BooleanToCNF.cpp
@@ -14,79 +14,80 @@ using namespace std;
 
 
 
-int assertVectorsDiffer(SATSolver& mng, varDir& dir, int v1, int v2, int size){
-	int N = size;
-	int lastone = 0;
-	for(int i=0; i<N; ++i){		
-		int cur = dir.addXorClause(v1+i, v2+i);
-		if(lastone != 0){
-			lastone = dir.addOrClause(lastone, cur);
-		}else{
-			lastone = cur;
-		}		
-	}
-	return lastone;
+int
+varDir::assertVectorsDiffer (int v1, int v2, int size)
+{
+    int N = size;
+    int lastone = 0;
+    for(int i=0; i<N; ++i){		
+	int cur = addXorClause(v1+i, v2+i);
+	if(lastone != 0){
+	    lastone = addOrClause(lastone, cur);
+	}else{
+	    lastone = cur;
+	}		
+    }
+    return lastone;
 }
 
-
-int select(SATSolver& mng, varDir& dir, int choices[], int control, int nchoices, int bitsPerChoice){
-	int outvar = dir.getVarCnt();	
-	for(int i=0; i<bitsPerChoice; ++i){
-		dir.newAnonymousVar();
-	}	
-	for(int j=0; j<bitsPerChoice; ++j){
-		mng.setVarClause( -(dir.newAnonymousVar()));
-		for(int i=0; i<nchoices; ++i){
-			int cvar = dir.newAnonymousVar();
-			mng.addAndClause( cvar, control+i, choices[i]+j);
-			int cvar2 = dir.newAnonymousVar();
-			mng.addOrClause( cvar2, cvar, cvar-1);
-		}
-		mng.addEqualsClause( outvar+j, dir.getVarCnt()-1);
+int
+varDir::select (int choices[], int control, int nchoices, int bitsPerChoice)
+{
+    int outvar = getVarCnt();	
+    for(int i=0; i<bitsPerChoice; ++i){
+	newAnonymousVar();
+    }	
+    for(int j=0; j<bitsPerChoice; ++j){
+	mng.setVarClause( -(newAnonymousVar()));
+	for(int i=0; i<nchoices; ++i){
+	    int cvar = newAnonymousVar();
+	    mng.addAndClause( cvar, control+i, choices[i]+j);
+	    int cvar2 = newAnonymousVar();
+	    mng.addOrClause( cvar2, cvar, cvar-1);
 	}
-	return outvar;
+	mng.addEqualsClause( outvar+j, getVarCnt()-1);
+    }
+    return outvar;
 }
 
-
-int selectMinGood(SATSolver& mng, varDir& dir, int choices[], int control, int nchoices, int bitsPerChoice){
-	int outvar = select(mng, dir, choices, control, nchoices, bitsPerChoice);	
-	int differences = dir.getVarCnt();	
-	int prev = dir.newAnonymousVar();
-	mng.setVarClause(-prev);
-	for(int i=1; i<nchoices; ++i){
-		int different = assertVectorsDiffer(mng, dir, choices[i-1], choices[i], bitsPerChoice);
-		int cvar = dir.newAnonymousVar();
-		mng.addAndClause( cvar, control+i, different);
-		int cvar2 = dir.newAnonymousVar();
-		mng.addOrClause( cvar2, cvar, prev);
-		prev = cvar2;
-	}
-	mng.setVarClause( -prev);	
-	return outvar;
+int
+varDir::selectMinGood (int choices[], int control, int nchoices, int bitsPerChoice)
+{
+    int outvar = select(choices, control, nchoices, bitsPerChoice);	
+    int differences = getVarCnt();	
+    int prev = newAnonymousVar();
+    mng.setVarClause(-prev);
+    for(int i=1; i<nchoices; ++i){
+	int different = assertVectorsDiffer(choices[i-1], choices[i], bitsPerChoice);
+	int cvar = newAnonymousVar();
+	mng.addAndClause( cvar, control+i, different);
+	int cvar2 = newAnonymousVar();
+	mng.addOrClause( cvar2, cvar, prev);
+	prev = cvar2;
+    }
+    mng.setVarClause( -prev);	
+    return outvar;
 }
 
-
-
-
-int arbitraryPerm(SATSolver& mng, varDir& dir, int input, int insize, int controls[], int ncontrols, int csize){
-	// ncontrols = sizeof(controls);
-	Assert( insize <= csize, "This is an error");
-	int OUTSIZE = ncontrols;
-	int outvar = dir.getVarCnt(); for(int i=0; i<OUTSIZE; ++i){ dir.newAnonymousVar(); };
-	for(int i=0; i<OUTSIZE; ++i){
-		int cvarPrev = dir.newAnonymousVar();
-		mng.setVarClause( -cvarPrev);
-		for(int j=0; j<insize; ++j){
-			int cvar = dir.newAnonymousVar();
-			mng.addAndClause( cvar, controls[i]+j, input+j);
-			int cvar2 = dir.newAnonymousVar();
-			mng.addOrClause( cvar2, cvar, cvarPrev);
-			cvarPrev = cvar2;
-		}
-		mng.addEqualsClause( outvar+i, cvarPrev);
+int
+varDir::arbitraryPerm (int input, int insize, int controls[], int ncontrols, int csize)
+{
+    // ncontrols = sizeof(controls);
+    Assert( insize <= csize, "This is an error");
+    int OUTSIZE = ncontrols;
+    int outvar = getVarCnt(); for(int i=0; i<OUTSIZE; ++i){ newAnonymousVar(); };
+    for(int i=0; i<OUTSIZE; ++i){
+	int cvarPrev = newAnonymousVar();
+	mng.setVarClause( -cvarPrev);
+	for(int j=0; j<insize; ++j){
+	    int cvar = newAnonymousVar();
+	    mng.addAndClause( cvar, controls[i]+j, input+j);
+	    int cvar2 = newAnonymousVar();
+	    mng.addOrClause( cvar2, cvar, cvarPrev);
+	    cvarPrev = cvar2;
 	}
-	return outvar;
+	mng.addEqualsClause( outvar+i, cvarPrev);
+    }
+    return outvar;
 }
-
-
 
