685f0ac83f814d4f07f1250cab379bda77341c9c
asolar <none@none>
2006-11-17 13:21:20 -0800

    *** empty log message ***

--- src/SketchSolver/InputParser/InputReader.cpp
+++ src/SketchSolver/InputParser/InputReader.cpp
@@ -22,6 +22,8 @@ int isatty(int i);
   int global_filterid = 0;
   int NCTRLS=5;
   bool overrideNCtrls=false;
+  int NINPUTS=5;
+  bool overrideInputs=false;
 #include "InputLexer.c"
 #include "InputParser.c"
 
