8a24dc37cab3c62338507f39b0b04ecfa37cf14f
asolar <none@none>
2007-02-21 18:20:37 -0800

    Added an optimization phase.

---
+++ src/SketchSolver/InputParser/DagOptim.cpp
@@ -0,0 +1,385 @@
+#include "DagOptim.h"
+#include "SATSolver.h"
+
+DagOptim::DagOptim()
+{
+	rvalue = NULL;
+}
+
+DagOptim::~DagOptim()
+{
+}
+CONST_node* DagOptim::getCnode(int val){
+	if( cnmap.find(val) == cnmap.end() ){
+		CONST_node* cnode = new CONST_node(val);
+		cnode->id = newnodes.size() + dagsize;
+		newnodes.push_back(cnode);
+		cnmap[val] = cnode;
+		return cnode;
+	}else{
+		return cnmap[val];	
+	}
+}
+
+CONST_node* DagOptim::getCnode(bool c){
+	return getCnode( c ? 1 : 0 );
+}
+
+
+bool DagOptim::isNegOfEachOther(bool_node* n1, bool_node* n2){
+	if( n1->type == bool_node::NOT){
+		if( n1->mother == n2){
+			return true;	
+		}
+	}
+	
+	if( n2->type == bool_node::NOT){
+		if( n2->mother == n1){
+			return true;
+		}
+	}
+	return false;
+}
+bool DagOptim::isConst(bool_node* n1){
+	if( n1->type == bool_node::ARITH ){
+		arith_node* an = dynamic_cast<arith_node*>(n1);
+		if( an->arith_type == arith_node::CONST ){
+			return true;	
+		}	
+	}	
+	return false;
+}
+
+
+bool DagOptim::getBval(bool_node* n1){
+	return getIval(n1) != 0;
+}	
+
+int  DagOptim::getIval(bool_node* n1){
+	Assert( isConst(n1), "This node is not a constant !!");
+	CONST_node * cn = dynamic_cast<CONST_node*>(n1);
+	return cn->getVal()	;
+}
+
+
+void DagOptim::visit( SRC_node& node ){
+	Dout( cout<<" node "<<node.get_name()<<endl );
+	rvalue = &node;
+}
+
+void DagOptim::visit( CTRL_node& node ){
+	rvalue = &node;
+}
+
+void DagOptim::visit( CONST_node& node ){
+	int val = node.getVal();
+	if( cnmap.find(val) == cnmap.end() ){
+		cnmap[val] = &node;
+		rvalue = &node;
+	}else{
+		rvalue = cnmap[val];	
+	}	
+}
+
+void DagOptim::visit( AND_node& node ){
+	if( node.father == node.mother ){ // x & x == x
+		rvalue = node.father;
+		return;	
+	}
+	if( isNegOfEachOther(node.father, node.mother) ){ // x & !x == false
+		rvalue = getCnode(0);
+		return;	
+	}
+	if( isConst(node.father) ){
+		if( !getBval(node.father) ){ // x & false == false;
+			rvalue = getCnode(0);
+			return;
+		}		
+		if( isConst(node.mother) ){ // const prop
+			rvalue = getCnode ( getBval( node.father ) && getBval( node.mother ) );				
+			return;
+		}
+	}
+	if( isConst(node.mother) ){
+		if( ! getBval(node.mother)){ // false & x == false;
+			rvalue = getCnode(0);			
+			return;
+		}
+	}
+	rvalue = &node;
+}
+
+void DagOptim::visit( OR_node& node ){
+	if( node.father == node.mother ){ // x | x == x
+		rvalue = node.father;
+		return;	
+	}
+	if( isNegOfEachOther(node.father, node.mother) ){ // x | !x == true
+		rvalue = getCnode(1);
+		return;	
+	}
+	if( isConst(node.father) ){
+		if( getBval(node.father) ){ // x | true == true
+			rvalue = getCnode(1);			
+			return;
+		}
+		if( isConst(node.mother) ){ // const prop
+			rvalue = getCnode ( getBval( node.father ) || getBval( node.mother ) );				
+			return;
+		}
+	}
+	if( isConst(node.mother) ){ // true | x == true
+		if( getBval(node.mother)){
+			rvalue = getCnode(1);			
+			return;
+		}
+	}
+	rvalue = &node;
+}
+
+
+void DagOptim::visit( XOR_node& node ){
+	if( node.father == node.mother ){ // x ^ x == false
+		rvalue = getCnode(0);
+		return;	
+	}
+	if( isNegOfEachOther(node.father, node.mother) ){ // x ^ !x == true
+		rvalue = getCnode(1);
+		return;	
+	}
+	if( isConst(node.father) ){
+		if( isConst(node.mother)){ // const prop
+			rvalue  = getCnode( getBval( node.father ) != getBval( node.mother ) );
+			return;
+		}
+	}
+	
+	rvalue = &node;
+}
+
+void DagOptim::visit( NOT_node& node ){
+	if( isConst(node.mother) ){ // const prop
+		rvalue = getCnode( ! getBval(node.mother) );			
+		return;
+	}
+	if( node.mother->type == bool_node::NOT){// ! ! x == x;
+		rvalue = node.mother->mother;
+		return;
+	}
+	rvalue = &node;
+}
+
+
+void DagOptim::visit( PLUS_node& node ){
+	if( isConst(node.mother) ){ // const prop
+		if( isConst(node.father) ){
+			rvalue  = getCnode( getIval( node.mother ) + getIval( node.father ) );
+			return;
+		}			
+	}
+	rvalue = &node;
+}
+
+void DagOptim::visit( TIMES_node& node ){
+	if( isConst(node.mother) ){ // const prop
+		if( isConst(node.father) ){
+			rvalue  = getCnode( getIval( node.mother ) * getIval( node.father ) );
+			return;
+		}
+		if(  getIval( node.mother ) == 0 ){
+			rvalue  = getCnode( 0 );
+			return;
+		}
+		if(  getIval( node.mother ) == 1 ){
+			rvalue  = node.father;
+			return;
+		}	
+	}
+	if( isConst(node.father) ){
+		if(  getIval( node.father ) == 0 ){
+			rvalue  = getCnode( 0 );
+			return;
+		}
+		if(  getIval( node.father ) == 1 ){
+			rvalue  = node.mother;
+			return;
+		}
+	}
+	rvalue = &node;
+}
+
+void DagOptim::visit( DIV_node& node ){
+	if( isConst(node.mother) ){ // const prop
+		if( isConst(node.father) ){
+			rvalue  = getCnode( getIval( node.mother ) / getIval( node.father ) );
+			return;
+		}			
+	}
+	rvalue = &node;
+}
+void DagOptim::visit( MOD_node& node ){
+	if( isConst(node.mother) ){ // const prop
+		if( isConst(node.father) ){
+			rvalue  = getCnode( getIval( node.mother ) % getIval( node.father ) );
+			return;
+		}			
+	}
+	rvalue = &node;
+}
+void DagOptim::visit( NEG_node& node ){
+	if( isConst(node.mother) ){	// const prop
+		rvalue  = getCnode( -getIval( node.mother ) );
+		return;
+	}
+	if( typeid(*(node.mother)) == typeid(node)  ){// - -x == x;
+		rvalue = node.mother->mother;
+		return;
+	}
+	rvalue = &node;
+}
+	
+void DagOptim::visit( GT_node& node ){
+	if( isConst(node.mother) ){
+		if( isConst(node.father) ){
+			rvalue  = getCnode( getIval( node.mother ) > getIval( node.father ) );
+			return;
+		}			
+	}
+	
+	if( node.mother == node.father ){
+		rvalue = getCnode(false);
+		return;
+	} 
+	rvalue = &node;
+}
+void DagOptim::visit( GE_node& node ){
+	if( isConst(node.mother) ){
+		if( isConst(node.father) ){
+			rvalue  = getCnode( getIval( node.mother ) >= getIval( node.father ) );
+			return;
+		}			
+	}
+	if( node.mother == node.father ){
+		rvalue = getCnode(true);
+		return;
+	} 
+	rvalue = &node;
+}
+void DagOptim::visit( LT_node& node ){
+	if( isConst(node.mother) ){
+		if( isConst(node.father) ){
+			rvalue  = getCnode( getIval( node.mother ) < getIval( node.father ) );
+			return;
+		}			
+	}
+	if( node.mother == node.father ){
+		rvalue = getCnode(false);
+		return;
+	} 
+	rvalue = &node;
+}
+
+void DagOptim::visit( LE_node& node ){
+	if( isConst(node.mother) ){
+		if( isConst(node.father) ){
+			rvalue  = getCnode( getIval( node.mother ) <= getIval( node.father ) );
+			return;
+		}			
+	}
+	if( node.mother == node.father ){
+		rvalue = getCnode(true);
+		return;
+	} 
+	rvalue = &node;
+}
+void DagOptim::visit( EQ_node& node ){
+	if( isConst(node.mother) ){
+		if( isConst(node.father) ){
+			rvalue  = getCnode( getIval( node.mother ) == getIval( node.father ) );
+			return;
+		}			
+	}
+	if( node.mother == node.father ){
+		rvalue = getCnode(true);
+		return;
+	} 
+	rvalue = &node;
+}
+
+void DagOptim::visit( UFUN_node& node ){
+	rvalue = &node;	
+}
+
+void DagOptim::visit( ARRACC_node& node ){
+	if( isConst(node.mother) ){
+		int val = getIval( node.mother );		
+		if(val < node.multi_mother.size()){
+			rvalue = node.multi_mother[val];
+		}else{
+			rvalue = getCnode(0);
+		}
+		return;
+	}
+	bool tmp = true;
+	int i=0;
+	if( i<node.multi_mother.size() ){
+		bool_node* bn =  node.multi_mother[0];
+		for(i=1; i<node.multi_mother.size(); ++i){
+			if( bn != node.multi_mother[i] ){
+				tmp = false;
+			}
+		}
+		if( tmp ){
+			rvalue = bn;
+			return;	
+		}
+	}
+	rvalue = &node;
+}
+void DagOptim::visit( ARRASS_node& node ){	
+	rvalue = &node;	
+}
+void DagOptim::visit( ACTRL_node& node ){
+	rvalue = &node;
+}
+	
+void DagOptim::visit( ASSERT_node &node){
+	rvalue = &node;
+}	
+void DagOptim::visit( DST_node& node ){
+	rvalue = &node;
+}
+
+void DagOptim::process(BooleanDAG& dag){
+	dagsize = dag.size();
+	DagCSE cse(dag);
+	int k=0;
+	for(int i=0; i<dag.size(); ++i ){
+		// Get the code for this node. 
+		dag[i]->accept(*this);
+		bool_node* node = rvalue;
+		
+		node->accept(cse);
+		
+		// look it up in the cse map.		
+		Dout(cout<<dag[i]->id<<"  "<<dag[i]->get_name()<<"("<< node->get_name() <<"): "<<cse.ccode<<endl) ;
+		if( cse.cse_map.find(cse.ccode) != cse.cse_map.end() ){
+			// if we do find it, then remove the node and replace it with its cse.			
+			bool_node * csubexp = cse.cse_map[cse.ccode];
+			Dout(cout<<"replacing "<<dag[i]->get_name()<<" -> "<<csubexp->get_name()<<endl );
+			dag.replace(i, csubexp); 
+		}else{
+			// if we don't find it, just add it.
+			cse.cse_map[cse.ccode] = node;
+			if( dag[i] != node ){
+				Dout(cout<<"replacing "<<dag[i]->get_name()<<" -> "<<node->get_name()<<endl );
+				dag.replace(i, node);
+			}
+		}
+	}
+	dag.removeNullNodes();
+	dag.addNewNodes(newnodes);
+	dag.sort_graph();
+	dag.relabel();
+	Dout(cout<<" end cse "<<endl);
+}
---
+++ src/SketchSolver/InputParser/DagOptim.h
@@ -0,0 +1,57 @@
+#ifndef DAGOPTIM_H_
+#define DAGOPTIM_H_
+
+#include "BooleanDAG.h"
+#include "DagCSE.h"
+
+class DagOptim : public NodeVisitor
+{
+	int dagsize;
+public:
+	
+	vector<bool_node*> newnodes;
+	bool_node* rvalue;
+	DagOptim();
+	virtual ~DagOptim();
+	
+	virtual void visit( SRC_node& node );
+	virtual void visit( CTRL_node& node );
+	virtual void visit( CONST_node& node );
+	
+	virtual void visit( AND_node& node );
+	virtual void visit( OR_node& node );
+	virtual void visit( XOR_node& node );
+		
+	virtual void visit( NOT_node& node );
+	
+	virtual void visit( PLUS_node& node );
+	virtual void visit( TIMES_node& node );
+	virtual void visit( UFUN_node& node );
+	virtual void visit( ARRACC_node& node );
+	virtual void visit( DIV_node& node );
+	virtual void visit( MOD_node& node );
+	virtual void visit( NEG_node& node );
+	
+	virtual void visit( GT_node& node );
+	virtual void visit( GE_node& node );
+	virtual void visit( LT_node& node );
+	virtual void visit( LE_node& node );
+	virtual void visit( EQ_node& node );
+	virtual void visit( ARRASS_node& node );
+	virtual void visit( ACTRL_node& node );
+	
+	virtual void visit( ASSERT_node &node);	
+	virtual void visit( DST_node& node );
+	virtual void process(BooleanDAG& bdag);	
+	map<int, CONST_node*> cnmap;
+	virtual  CONST_node* getCnode(int c);
+	virtual  CONST_node* getCnode(bool val); 
+	virtual bool isNegOfEachOther(bool_node* n1, bool_node* n2); 
+	virtual bool isConst(bool_node* n1);
+	virtual bool getBval(bool_node* n1);
+	virtual int getIval(bool_node* n1);
+};
+
+
+
+#endif /*DAGOPTIM_H_*/
