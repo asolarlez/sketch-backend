6c6b95fb809b1d5145f5bb4405e9af5ec3786164
asolar <none@none>
2007-05-16 13:20:48 -0700

    Experimental. Handle with care.

---
+++ src/SketchSolver/InputParser/NodeVisitor.cpp
@@ -0,0 +1,102 @@
+#include "NodeVisitor.h"
+#include "BooleanDAG.h"
+
+NodeVisitor::NodeVisitor()
+{
+}
+
+NodeVisitor::~NodeVisitor()
+{
+}
+
+
+
+void NodeVisitor::process(BooleanDAG& bdag){
+	for(BooleanDAG::iterator node_it = bdag.begin(); node_it != bdag.end(); ++node_it){
+		try{
+		(*node_it)->accept(*this);
+		}catch(BasicError& be){
+			throw BasicError((*node_it)->get_name(), "ERROR WAS IN THE FOLLOWING NODE");      		
+    	}
+	}
+}
+
+
+void NodeVisitor::visitArith(arith_node& node ){
+	rvalue = &node;
+}
+
+void NodeVisitor::visitBool(bool_node& node ){
+	rvalue = &node;
+}
+
+void NodeVisitor::visit( AND_node& node ){
+	visitBool(node);
+}
+void NodeVisitor::visit( OR_node& node ){
+	visitBool(node);
+}
+void NodeVisitor::visit( XOR_node& node ){
+	visitBool(node);	
+}
+void NodeVisitor::visit( SRC_node& node ){
+	visitBool(node);	
+}
+void NodeVisitor::visit( DST_node& node ){
+	visitBool(node);	
+}
+void NodeVisitor::visit( NOT_node& node ){
+	visitBool(node);	
+}
+void NodeVisitor::visit( CTRL_node& node ){
+	visitBool(node);	
+}
+void NodeVisitor::visit( PLUS_node& node ){
+	visitArith(node);	
+}
+void NodeVisitor::visit( TIMES_node& node ){
+	visitArith(node);	
+}
+void NodeVisitor::visit( UFUN_node& node ){
+	visitArith(node);	
+}
+void NodeVisitor::visit( ARRACC_node& node ){
+	visitArith(node);	
+}
+void NodeVisitor::visit( DIV_node& node ){
+	visitArith(node);	
+}
+void NodeVisitor::visit( MOD_node& node ){
+	visitArith(node);	
+}
+void NodeVisitor::visit( NEG_node& node ){
+	visitArith(node);	
+}
+void NodeVisitor::visit( CONST_node& node ){
+	visitArith(node);	
+}
+void NodeVisitor::visit( GT_node& node ){
+	visitArith(node);	
+}
+void NodeVisitor::visit( GE_node& node ){
+	visitArith(node);	
+}
+void NodeVisitor::visit( LT_node& node ){
+	visitArith(node);	
+}
+void NodeVisitor::visit( LE_node& node ){
+	visitArith(node);	
+}
+void NodeVisitor::visit( EQ_node& node ){
+	visitArith(node);	
+}
+void NodeVisitor::visit( ARRASS_node& node ){
+	visitArith(node);	
+}
+void NodeVisitor::visit( ACTRL_node& node ){
+	visitArith(node);	
+}
+void NodeVisitor::visit( ASSERT_node &node){
+	visitBool(node);	
+}	
+
