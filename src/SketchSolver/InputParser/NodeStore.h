d3cf815b49a1e1282c2b1b818336ba62697c151e
asolar <none@none>
2007-11-06 08:47:08 -0800

    Big optimization shaves about 30% from the circuit optimization time.

--- src/SketchSolver/InputParser/NodeStore.h
+++ src/SketchSolver/InputParser/NodeStore.h
@@ -5,6 +5,21 @@
 
 class NodeStore
 {
+	int timestamp;
 public:
+	NodeStore(){
+		timestamp = 0;
+	}
+	inline int getTimestamp(){
+		return ++timestamp;
+	}
+	inline void setTimestamp(bool_node* bn){
+		bn->layer = ++timestamp;
+	}
+	inline void setTimestampChildren(bool_node* bn){
+		for(child_iter it = bn->children.begin(); it != bn->children.end(); ++it){
+			(*it)->layer = ++timestamp;
+		}
+	}
 	virtual void addNode(bool_node* node)= 0;
 };
