41de49966a088c1076e36fa2fb9e17764d171068
asolar <none@none>
2008-02-06 11:12:41 -0800

    *** empty log message ***

--- src/SketchSolver/InputParser/NodeStore.cpp
+++ src/SketchSolver/InputParser/NodeStore.cpp
@@ -1,2 +1,8 @@
 #include "NodeStore.h"
 
+void NodeStore::addNode(bool_node* node){
+	node->id = newnodes.size() + dagsize;
+	Dout(cout<<" add "<<node->id<<"  "<<node->get_name()<<endl);
+	newnodes.push_back(node);
+}
+
