c45cd35c45a5ac034f1b7e963d65f6e5a140d00e
cgjones <none@none>
2008-02-08 19:09:48 -0800

    Made the final changes to the SKETCH build system.  This includes:
      * make sure headers are included in the dist
      * ditto for Java sources and the bit vector library
      * the 'sketch' script is now generated from an m4 template
      * ToSBit defaults the 'SBitII' path to ''

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
 ABC_GEN=gen_makefile_includes.sh
-
+SKETCH_GEN=gen_sketch_sources.sh
 
 cd backend/abc60513
 /bin/sh $ABC_GEN
 cd ../..
+
+cd frontend
+/bin/sh $SKETCH_GEN
+cd ../
 
 autoreconf --force --install
