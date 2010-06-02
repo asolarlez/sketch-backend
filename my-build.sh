4d815fd366ecfc95342ce7108953fe99aeadf2b1
sagarj <none@none>
2010-06-01 21:55:59 -0700

    Adding my build script for sketch project.
    --HG--
    branch : my-local

---
+++ my-build.sh
@@ -0,0 +1,6 @@
+cd $SKETCH/sketch-backend
+make -j2
+sudo make install
+cd $SKETCH/sketch-frontend
+make run-local-seq EXEC_ARGS="src/test/sk/seq/miniTest1.sk"
+make system-install DESTDIR=/usr/bin SUDOINSTALL=1
