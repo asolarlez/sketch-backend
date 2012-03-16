bb099843007c3e5714e3ea8fe395d0ec91cab69f
gatoatigrado <none@none>
2012-03-16 16:23:26 -0700

    [minor] fix build script

--- bindings/build.sh
+++ bindings/build.sh
@@ -5,11 +5,10 @@ make -j8 &&
 gcc src/SketchSolver/InputParser/CegisCApi.h && (
     cd bindings;
     (cd CegisCApi && c2hs ../../src/SketchSolver/InputParser/CegisCApi.h API.chs) &&
-    # cat CegisCApi.hs &&
     mkdir -p doc &&
     haddock --html -o doc CegisCApi/*.hs &&
-    # ./upload_doc.sh &&
-    echo -e "\n\n\n\nBindings compiled successfully! Type ':browse' to see exposed functions, or open up doc/index.html\n" &&
-    # ghci -L../src/SketchSolver/.libs -lcegis CegisCApi.API
-    ghci -L../src/SketchSolver/.libs -lcegis CegisCApi.Test
+    mkdir -p .libs &&
+    cp ../src/SketchSolver/.libs/libcegis.a .libs &&
+    mkdir -p dist &&
+    ghc -outputdir dist -o hscegis -O2 -optc-O3 -fforce-recomp -main-is HsCegis HsCegis -L.libs -lcegis -lstdc++
     )
