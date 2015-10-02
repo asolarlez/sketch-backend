35f43e47dd3d908dd5f30fa679dd5a59e419ab1e
Jinseong Jeon <jsjeon@cs.umd.edu>
2015-10-02 12:13:55 -0400

    discrete probability without modulo operator

--- src/SketchSolver/InputParser/DagFunctionInliner.cpp
+++ src/SketchSolver/InputParser/DagFunctionInliner.cpp
@@ -379,15 +379,18 @@ bool_node* HoleHardcoder::checkRandHole(CTRL_node* node, DagOptim& opt){
 					// cout<<"Single child is "<<bn->lprint()<<endl;
 				}
 			}
+			double p;
 			bool conc_flag;
 			if (node->is_sp_concretize()) {
-				conc_flag = (rand() % (int)odds == 0) || (chsize > 1500 && totsize < 10000) || (chsize > 5000);
+				p = 1.0 / odds;
+				conc_flag = (chsize > 1500 && totsize < 10000) || (chsize > 5000);
+				conc_flag = conc_flag || (rand() < p * ((double)RAND_MAX + 1.0));
 			} else {
-				double p = (1.0 / (1.0 + exp(-1.0/odds)) - 0.5) * 2.0;
-				conc_flag = ((double)rand() / RAND_MAX) <= p;
+				p = (1.0 / (1.0 + exp(-1.0/odds)) - 0.5) * 2.0;
+				conc_flag = (p > 0) && (rand() < p * ((double)RAND_MAX + 1.0));
 			}
 			if (conc_flag) {
-				cout<<node->get_name()<<" odds = 1/"<<odds<<"  ("<<chsize<<", "<<tchld<<") "<<" try to replace"<<endl;
+				cout<<node->get_name()<<" odds = 1/"<<odds<<" "<<p<<" ("<<chsize<<", "<<tchld<<") "<<" try to replace"<<endl;
 				int bound = 1;
 				
 				int nbits = node->get_nbits();
