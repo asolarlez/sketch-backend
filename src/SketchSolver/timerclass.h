41153b03e0b09938ed952264c5ac5479ff53982a
asolar <none@none>
2016-06-15 20:31:41 -0400

    Fixed compatibility bug.
    --HG--
    branch : SmartUfuns

--- src/SketchSolver/timerclass.h
+++ src/SketchSolver/timerclass.h
@@ -24,7 +24,7 @@ class timerclass{
 		ctime = 0;
 	}
 
-	gettimeofdayme(timeval* tv, timeval*) {
+	void gettimeofdayme(timeval* tv, timeval* emp) {
 		time_point<high_resolution_clock, milliseconds> msc= time_point_cast<milliseconds>(high_resolution_clock::now());
 		tv->tv_sec = msc.time_since_epoch().count() / 1000;
 		tv->tv_usec = msc.time_since_epoch().count() % 1000;
