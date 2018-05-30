8fb2501d8a9d596e400883c6642ed077904cbcdb
asolar <none@none>
2018-05-30 02:33:35 -0400

    Final stable version.

--- src/SketchSolver/InputParser/FloatSupport.h
+++ src/SketchSolver/InputParser/FloatSupport.h
@@ -53,8 +53,97 @@ float tlog(float in) {
 }
 
 
+class FloatManager {
+	map<float, int> floatIdx;
+	vector<float> floats;
+	map<string, floatfun> floatfuns;
+
+public:
+	const float epsilon;
+	FloatManager(float _epsilon) :epsilon(_epsilon) {
+		floatIdx[0.0] = 0;
+		floats.push_back(0.0);
+		floatfuns["arctan_math"] = atan;
+		floatfuns["sin_math"] = sin;
+		floatfuns["cos_math"] = cos;
+		floatfuns["tan_math"] = tan;
+		floatfuns["sqrt_math"] = sqrt;
+		floatfuns["log_math"] = tlog;
+	}
+
+	bool hasFun(const string& name) {
+		return floatfuns.count(name) > 0;
+	}
+	FloatFun getFun(const string& name) {
+		return FloatFun(*this, floatfuns[name]);
+	}
+
+	float operator()(int id) {
+		return getFloat(id);
+	}
+
+	float getFloat(int id) {
+		if (id < 0) {
+			return -floats[-id];
+		}
+		else {
+			return	floats[id];
+		}
+
+	}
+
+	int getIdx(float x) {
+		//floatIdx only stores positive values. Negative values will yield negative indices. 
+		//This means that negating the index will automatically negate the value that the index corresponds to.
+		//That's why zero must always be stored in index zero.
+		bool isNeg = false;
+		if (x < -0.0) {
+			x = -x;
+			isNeg = true;
+		}
+		auto lbd = floatIdx.lower_bound(x - epsilon + (epsilon / 100));
+
+		if (lbd != floatIdx.end()) {
+			float dist = lbd->first - x;
+			if (-epsilon < dist && dist < epsilon) {
+				if (isNeg) {
+					return -lbd->second;
+				}
+				else {
+					return  lbd->second;
+				}
+			}
+			lbd++;
+			if (lbd != floatIdx.end()) {
+				dist = lbd->first - x;
+				if (-epsilon < dist && dist < epsilon) {
+					if (isNeg) {
+						return -lbd->second;
+					}
+					else {
+						return  lbd->second;
+					}
+				}
+			}
+		}
 
 
+		int pos = floatIdx.size();
+		floatIdx[x] = pos;
+		floats.push_back(x);
+		Assert(floatIdx.size() == floats.size(), "What???");
+		if (isNeg) {
+			return -pos;
+		}
+		else {
+			return pos;
+		}
+	}
+};
+
+
+
+/*
 
 class FloatManager {	
 	vector<uint64_t> floats;
@@ -166,50 +255,9 @@ public:
 		Assert(false, "NO MORE ROOM!! size="<<count);
 		return -1;
 		
-
-		/*
-
-		auto lbd = floatIdx.lower_bound(x - epsilon + (epsilon / 100));
-
-		if (lbd != floatIdx.end()) {
-			float dist = lbd->first - x;
-			if (-epsilon < dist && dist < epsilon) {
-				if (isNeg) {
-					return -lbd->second;
-				}
-				else {
-					return  lbd->second;
-				}
-			}
-			lbd++;
-			if (lbd != floatIdx.end()) {
-				dist = lbd->first - x;
-				if (-epsilon < dist && dist < epsilon) {
-					if (isNeg) {
-						return -lbd->second;
-					}
-					else {
-						return  lbd->second;
-					}
-				}
-			}
-		}
-		
-
-
-		int pos = floatIdx.size();
-		floatIdx[x] = pos;
-		floats.push_back(x);
-		Assert(floatIdx.size() == floats.size(), "What???");
-		if (isNeg) {
-			return -pos;
-		}
-		else {
-			return pos;
-		}
-		*/
 	}
 };
+*/
 
 class FloatComp {
 protected:
